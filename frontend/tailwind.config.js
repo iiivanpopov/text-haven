887aeabcff81143b5d9c3a8f73bdf45d335c4a98
iiivanpopov <iiivanpopov999@gmail.com>
2025-03-31 20:57:23 +0300

    chore: set tailwind colors configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
+
+	theme: {
+		fontSize: {
+			heading: '6rem',
+			subheading: '3rem',
+			primary: '2rem',
+		},
+		colors: {
+			text: {
+				primary: '#1F2024',
+				secondary: '#282828',
+			},
+			button: {
+				blue: '#4484D7',
+				green: '#59CD6A',
+				red: '#EA5750',
+			},
+			background: {
+				primary: '#F3F3F1',
+				secondary: '#F1F1F1',
+			},
+			outline: {
+				DEFAULT: '#DDDDDD',
+			},
+		},
+	},
+}
