13fa3a1f4980fc0ef63d35eddf67d740f192b253
iiivanpopov <iiivanpopov999@gmail.com>
2025-04-15 12:42:20 +0300

    feat(ui): implement lazy loading for ModalRouter and add aria-labels to footer links

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,16 @@
-import ModalRouter from '@components/ModalRouter'
+import { lazy, Suspense } from 'react'
 import './globals.css'
+
+const LazyModalRouter = lazy(() => import('@components/ModalRouter'))
 
 export default async function Layout({ children }: React.PropsWithChildren) {
 	return (
 		<html lang='en'>
 			<body className='bg-gray-100 dark:bg-gray-950 min-h-screen text-gray-800 dark:text-gray-100 font-urbanist'>
 				{children}
-				<ModalRouter />
+				<Suspense fallback={null}>
+					<LazyModalRouter />
+				</Suspense>
 			</body>
 		</html>
 	)
