b584fbf6b1453e4de5ba7b19eee18624f7d2377f
iiivanpopov <iiivanpopov999@gmail.com>
2025-04-18 16:19:39 +0300

    feat(ui): filter files / folders

--- TODO.md
+++ TODO.md
@@ -4,11 +4,9 @@
 
 ### Frontend
 
-- Implement search for folders/files in user storage
 - Integrate Redux Toolkit for state management
 - Connect frontend to backend
 - Optimize frontend components
-
 - Dockerize
 
 ### Backend
@@ -17,14 +15,16 @@
 - Add endpoint to fetch last 3-4 posts (for main page display)
 - Add private/public account toggle (controls visibility of email & posts)
 - Improve caching mechanisms
-- Add unique username field to user model
 - Update endpoints for retrieving files/folders
 - Refactor to use Bun's built-in Redis and AWS S3 (remove external dependencies)
 
 ## WIP (Work in Progress)
 
+- Add unique username field to user model
+
 ## DONE
 
+- Implement search for folders/files in user storage
 - Create files/folders (UI only)
 - Edit texts (UI only)
 - Footer (with auth buttons)
--- frontend/src/app/storage/[id]/page.tsx
+++ frontend/src/app/storage/[id]/page.tsx
@@ -1,72 +1,99 @@
-// SOme mock data from api
+'use client'
+
+import Input from '@components/shared/Input'
+import { use, useEffect, useMemo, useState } from 'react'
+import CreateNew from '../components/CreateNew'
+import FileBtn from '../components/FileBtn'
+import FolderBtn from '../components/FolderBtn'
+
+type ShittyAPIREsponse = {
+	folders: {
+		name: string
+		id: string
+	}[]
+	files: {
+		name: string
+		id: string
+	}[]
+}
+
 const fakeFileSystem: Record<
-  string,
-  {
-    folders: { name: string; id: string }[];
-    files: { name: string; id: string; content: string }[];
-  }
+	string,
+	{
+		folders: { name: string; id: string }[]
+		files: { name: string; id: string; content: string }[]
+	}
 > = {
-  "": {
-    folders: [
-      { name: "Folder A", id: "a" },
-      { name: "Folder B", id: "b" },
-    ],
-    files: [
-      { name: "Root File 1", id: "a", content: "Content 1" },
-      { name: "Root File 2", id: "b", content: "Content 1" },
-    ],
-  },
-  a: {
-    folders: [{ name: "Folder C", id: "c" }],
-    files: [
-      { name: "File 1", id: "File 1", content: "Content 1" },
-      { name: "File 2", id: "File 2", content: "Content 1" },
-    ],
-  },
-  b: {
-    folders: [],
-    files: [
-      { name: "File 3", id: "File 3", content: "Content 1" },
-      { name: "File 4", id: "File 4", content: "Content 1" },
-    ],
-  },
-  c: {
-    folders: [],
-    files: [
-      { name: "File 5", id: "File 5", content: "Content 1" },
-      { name: "File 6", id: "File 6", content: "Content 1" },
-    ],
-  },
-};
+	'': {
+		folders: [
+			{ name: 'Folder A', id: 'a' },
+			{ name: 'Folder B', id: 'b' },
+		],
+		files: [
+			{ name: 'Root File 1', id: 'root-file-1', content: 'Content 1' },
+			{ name: 'Root File 2', id: 'root-file-2', content: 'Content 2' },
+		],
+	},
+	a: {
+		folders: [{ name: 'Folder C', id: 'c' }],
+		files: [
+			{ name: 'File 1', id: 'file-1', content: 'Content 1' },
+			{ name: 'File 2', id: 'file-2', content: 'Content 2' },
+		],
+	},
+	b: {
+		folders: [],
+		files: [
+			{ name: 'File 3', id: 'file-3', content: 'Content 3' },
+			{ name: 'File 4', id: 'file-4', content: 'Content 4' },
+		],
+	},
+}
 
-import { use } from "react";
-import CreateNew from "../components/CreateNew";
-import List from "../components/List";
+function escapeRegExp(string: string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
 
-export default function Storage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id: folderId } = use(params);
+export default function Storage({ params }: { params: Promise<{ id: string }> }) {
+	const { id: folderId } = use(params)
 
-  const data: {
-    folders: { name: string; id: string }[];
-    files: { name: string; id: string }[];
-  } = fakeFileSystem[folderId] ?? { folders: [], files: [] };
+	const [search, setSearch] = useState('')
 
-  // FIXME: Not an efficient way(AT ALL). Maybe i should save it at state manager??(redux/mobx)
-  const currentFolder = Object.values(fakeFileSystem)
-    .map((value) => value.folders?.find((val) => val.id == folderId))
-    .find((folder) => folder != undefined);
+	const [{ files, folders }, setData] = useState<ShittyAPIREsponse>({ folders: [], files: [] })
 
-  return (
-    <main className="mt-20 w-3/5 pb-40">
-      <h3 className="mb-5 text-5xl flex flex-col text-gray-800 dark:text-gray-100">
-        Storage {currentFolder?.name ? `| ${currentFolder.name}` : null}
-      </h3>
-      <List data={data} />
-      <CreateNew />
-    </main>
-  );
+	useEffect(() => {
+		setData(fakeFileSystem[folderId] ?? { folders: [], files: [] })
+	}, [folderId])
+
+	const escapedSearch = useMemo(() => escapeRegExp(search), [search])
+	const regex = useMemo(() => new RegExp(escapedSearch, 'i'), [escapedSearch])
+
+	const filteredFolders = useMemo(
+		() => folders.filter(folder => regex.test(folder.name)),
+		[folders, regex]
+	)
+
+	const filteredFiles = useMemo(() => files.filter(file => regex.test(file.name)), [files, regex])
+
+	return (
+		<main className='mt-20 w-3/5 pb-40'>
+			<h3 className='mb-5 text-5xl flex flex-col text-gray-800 dark:text-gray-100'>Storage</h3>
+			<Input
+				name='Search'
+				value={search}
+				onChange={e => setSearch(e.target.value)}
+				placeholder='Search...'
+			/>
+
+			{filteredFolders.map(folder => (
+				<FolderBtn key={folder.id} folderId={folder.id} className='px-5' title={folder.name} />
+			))}
+
+			{filteredFiles.map(file => (
+				<FileBtn key={file.id} className='px-5' fileId={file.id} title={file.name} />
+			))}
+
+			<CreateNew />
+		</main>
+	)
 }
